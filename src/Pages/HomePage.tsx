82d6cb599c180b64d47c56af8dc95b9bc48c23c8
MaiFahed <maifahed16@gmail.com>
2025-03-10 16:27:47 +0200

    HomePage

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import Main from "../Components/main";
+// import pillars from "../assets/pillars.jpg"
+
+function HomePage(){
+  return (
+    <Main />
+    // <div
+    //   style={{
+    //     backgroundImage: `url(${pillars})`,
+    //     backgroundSize: "cover",
+    //     backgroundPosition: "center",
+    //     backgroundRepeat: "no-repeat",
+    //     height: "100vh",
+    //     width: "100%",
+    //   }}
+    // />
+  );
+};
+
+export default HomePage;
